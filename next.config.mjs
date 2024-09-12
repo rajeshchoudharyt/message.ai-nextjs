62366a075b3563a9390fe5087428c38cfa967d67
Rajesh T <121572349+rajeshchoudharyt@users.noreply.github.com>
2024-09-12 09:18:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = { 
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  },
+};
 
 export default nextConfig;
